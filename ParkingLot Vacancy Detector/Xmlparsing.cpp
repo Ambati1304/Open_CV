f1673fc0d5424ceed1aa0143246612a06383ce3a
Saikiran Ambati <ambati1995@gmail.com>
2017-10-10 09:19:15 -0500

    Setting Environment for XMl parser
    adding important include directories and importing headers for xml
    parsing

---
+++ ParkingLot Vacancy Detector/Xmlparsing.cpp
@@ -0,0 +1,28 @@
+#include<rapidxml\rapidxml_utils.hpp>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include<opencv\cv.hpp>
+#include <opencv2/core/core.hpp>
+#include "opencv2/objdetect/objdetect.hpp"
+#include <opencv2/highgui/highgui.hpp>
+#include "Xmlparsing.h"
+
+using namespace std;
+using namespace rapidxml;
+using namespace cv;
+cv::RotatedRect boundbox;
+Mat  detectedMask;
+
+void Xmlparsing::readFile(string filename,Mat mask) {
+
+		
+
+		}
+
+
+
+	double Xmlparsing::test(RotatedRect annotBox) {
+		
+			
+	}
