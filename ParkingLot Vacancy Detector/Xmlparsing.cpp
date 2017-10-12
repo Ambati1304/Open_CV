2b0bb14b27f35d5613e12b0995899eb625f6bdc5
Saikiran Ambati <ambati1995@gmail.com>
2017-10-12 15:49:41 -0500

    Parse XML
    This section consists of parsing the required content out of xml, here
    all we need is Count of True Positive, False Positive, True Negative and
    accuracy, which are computed by knowing the occupied slots in Image
    which can be known from XML ID.
    
    Accuracy is calculated as (TP+TN)/TotalCount

--- ParkingLot Vacancy Detector/Xmlparsing.cpp
+++ ParkingLot Vacancy Detector/Xmlparsing.cpp
@@ -34,6 +34,78 @@ void Xmlparsing::readFile(string filename,Mat mask) {
 		exit(0);
 	}
 
+	//parse the document
+	string content(buffer.str());
+	doc.parse<0>(&content[0]);
+
+
+	int total_count = 0;
+	int truePositive = 0;
+	int falsePositive = 0;
+	int trueNegative = 0;
+	xml_node<> *pRoot = doc.first_node();
+	for (xml_node<> *pNode = pRoot->first_node("space"); pNode; pNode = pNode->next_sibling())
+	{
+		double occupany;
+		int occupied1 = 0, nValue = 0, width1 = 0, height1 = 0, x_point = 0, y_point = 0, centerX = 0, centerY = 0, angle = 0;
+		char line[30] = "\0";
+		xml_node<> *rotatedRect = pNode->first_node("rotatedRect");
+		xml_attribute<> *occupied = pNode->first_attribute("occupied");
+		xml_node<> *center_node = rotatedRect->first_node("center");
+		xml_attribute<> *center_x = center_node->first_attribute("x");
+		xml_attribute<> *center_y = center_node->first_attribute("y");
+		xml_node<> *size = rotatedRect->first_node("size");
+		xml_attribute<> *width = size->first_attribute("w");
+		xml_attribute<> *height = size->first_attribute("h");
+		xml_node<> *angleNode = rotatedRect->first_node("angle");
+		xml_attribute<> * angleAttri = angleNode->first_attribute("d");
+		xml_attribute<> *pAttr = center_node->first_attribute("y");
+		xml_node<> *contour_node = pNode->first_node("contour");
+		xml_node<> *points = contour_node->first_node("point");
+		xml_attribute<> *x = points->first_attribute("x");
+		xml_attribute<> *y = points->first_attribute("y");
+
+
+		if (occupied != nullptr && pAttr != nullptr&&width != nullptr&&height != nullptr&&x != nullptr&&y != nullptr) {
+			occupied1 = atoi(occupied->value());
+			nValue = atoi(pAttr->value());
+			width1 = atoi(width->value());
+			height1 = atoi(height->value());
+			x_point = atoi(x->value());
+			y_point = atoi(y->value());
+			centerX = atoi(center_x->value());
+			centerY = atoi(center_y->value());
+			angle = atoi(angleAttri->value());
+
+
+
+			boundbox = cv::RotatedRect(Point2f(centerX, centerY), Size2f(width1, height1), angle);
+			occupany = test(boundbox);
+			total_count++;
+		}
+
+		if (occupied1 == 1 && occupany > threshold) {
+			truePositive++;
+		}
+		else if (occupied1 == 0 && width1 >0 && height1>0 && occupany >threshold)
+		{
+			falsePositive++;
+		}
+		else if (occupied1 == 0 && width1 >0 && height1>0 && occupany <threshold)
+		{
+			trueNegative++;
+		}
+
+	}
+
+	cout << "True Postitive " << truePositive << endl;
+	cout << "False Postitive " << falsePositive << endl;
+	cout << "True Negative " << trueNegative << endl;
+	cout << "Total Count" << total_count << endl;
+	cout << "Accuracy " << 100 * (truePositive + trueNegative) / total_count << endl;
+
+
+
 		}
 
 
