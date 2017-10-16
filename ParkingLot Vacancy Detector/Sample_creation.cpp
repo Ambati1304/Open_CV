c602817cb5a96dcc294d42d25fc4997a17453e08
Saikiran Ambati <ambati1995@gmail.com>
2017-10-16 08:57:44 -0500

    Main Sample Creation
    we develop the maiin function which calls getfiles and open text files
    and saves the training information. We will implement the reading xml and
    getfiles function

--- ParkingLot Vacancy Detector/Sample_creation.cpp
+++ ParkingLot Vacancy Detector/Sample_creation.cpp
@@ -15,3 +15,22 @@ void readFile(fs::path, fs::path);
 void getFiles(fs::path);
 
 ofstream pos_training_set, neg_training_set;
+
+int main() {
+
+	pos_training_set.open("positive.txt");
+	neg_training_set.open("negative.txt");
+
+	//Get the root node
+	string currentpath = boost::filesystem::current_path().string();
+	cout << currentpath;
+	fs::path root[3] = { "D:\Masters\Spring'17\Computer Vision\Assignment 2\PKLot-small\PKLot\parking2\sunny","D:\Masters\Spring'17\Computer Vision\Assignment 2\PKLot-small\PKLot\parking1a\sunny","D:\Masters\Spring'17\Computer Vision\Assignment 2\PKLot-small\PKLot\parking1b\sunny" };
+	for (int i = 0; i < 3; i++)
+	{
+		getFiles(root[i]);
+	}
+
+	pos_training_set.close();
+	neg_training_set.close();
+	return 0;
+}
