cf9a56b5304835ff528032c24396079ad701c1b0
Saikiran Ambati <ambati1995@gmail.com>
2017-10-22 11:01:29 -0500

    Detecting Face nodes

--- Swapping Face through Image Blending/main.py
+++ Swapping Face through Image Blending/main.py
@@ -8,3 +8,27 @@ import numpy
 import sys
 from PIL import Image
 import matplotlib.pyplot as plt
+
+# In[95]:
+
+PREDICTOR_PATH = "shape_predictor_68_face_landmarks.dat"
+SCALE_FACTOR = 1
+FEATHER_AMOUNT = 11
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(PREDICTOR_PATH)
+FACE_POINTS = list(range(17, 68))
+MOUTH_POINTS = list(range(48, 61))
+RIGHT_BROW_POINTS = list(range(17, 22))
+LEFT_BROW_POINTS = list(range(22, 27))
+RIGHT_EYE_POINTS = list(range(36, 42))
+LEFT_EYE_POINTS = list(range(42, 48))
+NOSE_POINTS = list(range(27, 35))
+JAW_POINTS = list(range(0, 17))
+# Points used to line up the images.
+ALIGN_POINTS = (LEFT_BROW_POINTS + RIGHT_EYE_POINTS + LEFT_EYE_POINTS +
+                               RIGHT_BROW_POINTS + NOSE_POINTS + MOUTH_POINTS)
+OVERLAY_POINTS = [
+   LEFT_EYE_POINTS + RIGHT_EYE_POINTS + LEFT_BROW_POINTS + RIGHT_BROW_POINTS,
+    NOSE_POINTS + MOUTH_POINTS,
+]
+COLOUR_CORRECT_BLUR_FRAC = .8
