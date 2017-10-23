c1aed6d38c3b3da0ac085224c354c209121dc59f
Saikiran Ambati <ambati1995@gmail.com>
2017-10-23 08:01:29 -0500

    Face Detector Haar Cascade

--- Swapping Face through Image Blending/main.py
+++ Swapping Face through Image Blending/main.py
@@ -32,3 +32,53 @@ OVERLAY_POINTS = [
     NOSE_POINTS + MOUTH_POINTS,
 ]
 COLOUR_CORRECT_BLUR_FRAC = .8
+
+
+
+# In[96]:
+
+#haar face detector
+def haarFaceDetector(image):
+    face_cascade = cv2.CascadeClassifier('myfacedetector.xml')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    return faces
+#get the images and landmark
+def readImageAndLandmarks(fname):
+    image = cv2.imread(fname, cv2.IMREAD_COLOR)
+    image = cv2.resize(image, (image.shape[1] * SCALE_FACTOR,
+                         image.shape[0] * SCALE_FACTOR))
+    lMarks = getLandmarks(image)
+    return image, lMarks
+
+#color cohesion technique to correct the color
+def colorCohesion(image1, image2, landmarks1):
+    blur_amount = COLOUR_CORRECT_BLUR_FRAC * numpy.linalg.norm(
+                              numpy.mean(landmarks1[LEFT_EYE_POINTS], axis=0) -
+                              numpy.mean(landmarks1[RIGHT_EYE_POINTS], axis=0))
+    blur_amount = int(blur_amount)
+    if blur_amount % 2 == 0:
+        blur_amount += 1
+    image1_blur = cv2.GaussianBlur(image1, (blur_amount, blur_amount), 0)
+    image2_blur = cv2.GaussianBlur(image2, (blur_amount, blur_amount), 0)
+    image2_blur += (128 * (image2_blur <= 1.0)).astype(image2_blur.dtype)
+    return (image2.astype(numpy.float64) * image1_blur.astype(numpy.float64) /
+                                                image2_blur.astype(numpy.float64))
+
+def getLandmarks(im):
+    rects = detector(im, 1)
+    
+    if len(rects) > 1:
+        raise TooManyFaces
+    if len(rects) == 0:
+        raise NoFaces
+
+    return numpy.matrix([[p.x, p.y] for p in predictor(im, rects[0]).parts()])
+
+#get the Convex Hull
+def drawConvexHull(image, points, color):
+    points = cv2.convexHull(points)
+    cv2.fillConvexPoly(image, points, color=color)
+
+
+
