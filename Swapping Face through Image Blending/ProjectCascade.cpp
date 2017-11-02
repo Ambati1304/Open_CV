2f6af4cf17f6a5876f8ef2fd12e88d16e5b6c15b
Saikiran Ambati <ambati1995@gmail.com>
2017-11-02 18:44:46 -0500

    Facial_Detection

--- Swapping Face through Image Blending/ProjectCascade.cpp
+++ Swapping Face through Image Blending/ProjectCascade.cpp
@@ -7,3 +7,20 @@ void facialdetection(IplImage *faceframe);
 int value;
 int imgnum;
 int fiximagesnumber = 20;
+
+
+
+
+void facialdetection(IplImage *faceframe)
+{
+	CvSeq *faces = cvHaarDetectObjects(faceframe, facecascade, memory, 1.15, 5,
+		0, cvSize(30, 30));
+	for (int i = 0; i < (faces ? faces->total : 0); i++)
+	{
+		CvRect *r = (CvRect *)cvGetSeqElem(faces, i);
+		cvRectangle(faceframe,
+			cvPoint(r->x, r->y),
+			cvPoint(r->x + r->width, r->y + r->height),
+			CV_RGB(0, 0, 255), 2, 8, 0);
+	}
+}
